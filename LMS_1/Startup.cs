0f4c822b1a5a22a132db1cf0228469b335d03ea0
hanwen zhang <hanwen8102@outlook.com>
2019-02-25 13:28:16 +0100

    Add project files.

---
+++ LMS_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMS_1.Startup))]
+namespace LMS_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
